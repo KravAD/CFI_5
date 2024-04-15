bc74d22b399a1dd33716370761ed1d510a6398e0
KravAD <dkrav@myuax.com>
2024-04-15 12:48:41 +0200

    commit 6
    El método de contar genes tomo una cadena String de ADN como argumento y devuelve el número de dicha cadena. Primero se verifica si la cadena de ADN contiene al menos 3 caracteres que forman una secuencia de ADN. Luego verifica si la cadena empieza por ATG, en caso afirmativo empieza a contar a partir del carácter 3 y en caso contrario se salta el primer carácter.

--- AnalisisGenomicoYOrganizacionDatos/src/main/java/AnalisisGenomico/ContadorGenes.java
+++ AnalisisGenomicoYOrganizacionDatos/src/main/java/AnalisisGenomico/ContadorGenes.java
@@ -1,5 +1,12 @@
 package AnalisisGenomico;
 
 public class ContadorGenes {
-    
+    public int contarGenes(String dna) {
+        if (dna.length() < 3) return 0;
+        if (dna.startsWith("ATG")) {
+            return 1 + contarGenes(dna.substring(3));
+        } else {
+            return contarGenes(dna.substring(1));
+        }
+    }
 }
--- AnalisisGenomicoYOrganizacionDatos/src/main/java/org/example/Main.java
+++ AnalisisGenomicoYOrganizacionDatos/src/main/java/org/example/Main.java
@@ -1,8 +1,11 @@
 package org.example;
 import IU.InterfazUsuario;
-import AnalisisGenomico.*;
 
 public class Main {
     public static void main(String[] args) {
+        String dna = "ATGCGTATGAGCTAGCTAGCATG";
+        String gen1 = "ATG";
+        String gen2 = "CGT";
+        new InterfazUsuario(dna, gen1, gen2);
     }
 }
