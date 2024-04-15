b0e2f6c055a68464628bebff2b9b9900acea2623
KravAD <dkrav@myuax.com>
2024-04-15 21:35:32 +0200

    commit 14
    Actualización de la interfaz de usuario. Añado un southPanel con dos botones.

--- AnalisisGenomicoYOrganizacionDatos/src/main/java/IU/InterfazUsuario.java
+++ AnalisisGenomicoYOrganizacionDatos/src/main/java/IU/InterfazUsuario.java
@@ -1,5 +1,6 @@
 package IU;
 import AnalisisGenomico.*;
+import AnalisisNumerico.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,16 +11,25 @@ import java.util.List;
 public class InterfazUsuario extends JFrame{
     private ContadorGenes contadorGenes;
     private CalculoCombGeneticas calculoCombGeneticas;
+    private SumYListaNum sumYListaNum;
+    private PotenciasMaximos potenciasMaximos;
 
     public InterfazUsuario(String dna, String gen1, String gen2) {
         contadorGenes = new ContadorGenes();
         calculoCombGeneticas = new CalculoCombGeneticas();
+        sumYListaNum = new SumYListaNum();
+        potenciasMaximos = new PotenciasMaximos();
 
         JPanel panel = new JPanel(new BorderLayout());
         JPanel northPanel = new JPanel(new FlowLayout());
+        JPanel southPanel = new JPanel(new FlowLayout());
+
 
         JButton botonContarGenes = new JButton("Contar Genes");
         JButton botonCalcularCombinaciones = new JButton("Calcular Combinaciones");
+        JButton botonSumYListaNum = new JButton("SumYListaNum");
+        JButton botonPotenciasMaximos = new JButton("PotenciasMaximos");
+
 
         botonContarGenes.addActionListener(new ActionListener() {
             @Override
@@ -36,11 +46,34 @@ public class InterfazUsuario extends JFrame{
                 JOptionPane.showMessageDialog(null, "Combinaciones genéticas: " + combinaciones);
             }
         });
+        botonSumYListaNum.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int sum = sumYListaNum.sumatoria(10);
+                JOptionPane.showMessageDialog(null, "La suma de los primeros 10 números naturales es: " + sum);
+            }
+        });
+
+        botonPotenciasMaximos.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int[] numeros = {1, 3, 7, 2, 5};
+                int max = potenciasMaximos.maximo(numeros, 0);
+                JOptionPane.showMessageDialog(null, "El valor máximo en el array es: " + max);
+            }
+        });
 
         northPanel.add(botonContarGenes);
         northPanel.add(botonCalcularCombinaciones);
 
+
+        southPanel.add(botonSumYListaNum);
+        southPanel.add(botonPotenciasMaximos);
+
+
         panel.add(northPanel, BorderLayout.NORTH);
+        panel.add(southPanel, BorderLayout.SOUTH);
+
 
         this.add(panel);
         this.pack();
