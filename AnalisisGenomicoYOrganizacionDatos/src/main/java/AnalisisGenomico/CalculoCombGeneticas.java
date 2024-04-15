a08f7f3345be095a1490087f881d1d9256b9220d
KravAD <dkrav@myuax.com>
2024-04-15 13:02:07 +0200

    commit 7
    La clase tiene dos métodos. Calcular combinaciones es un método que crea una lista vacía de combinaciones de genes posibles, llama al método que hace el cálculo y luego devuelve las combinaciones. El método calcularCombinacionesRecursivo hace el cálculo de las combinaciones mediante la lectura del prefijo y el gen1 y gen2. Si gen1 y gen2 están vacíos, añade un prefijo, gen1 y gen2.

--- AnalisisGenomicoYOrganizacionDatos/src/main/java/AnalisisGenomico/CalculoCombGeneticas.java
+++ AnalisisGenomicoYOrganizacionDatos/src/main/java/AnalisisGenomico/CalculoCombGeneticas.java
@@ -1,4 +1,21 @@
 package AnalisisGenomico;
-
+import java.util.ArrayList;
+import java.util.List;
 public class CalculoCombGeneticas {
+    public List<String> calcularCombinaciones(String gen1, String gen2) {
+        List<String> combinaciones = new ArrayList<>();
+        calcularCombinacionesRecursivo("", gen1, gen2, combinaciones);
+        return combinaciones;
+    }
+
+    private void calcularCombinacionesRecursivo(String prefix, String gen1, String gen2, List<String> combinaciones) {
+        if (gen1.isEmpty() || gen2.isEmpty()) {
+            combinaciones.add(prefix + gen1 + gen2);
+            return;
+        }
+
+        for (int i = 0; i < gen2.length(); i++) {
+            calcularCombinacionesRecursivo(prefix + gen1.charAt(0) + gen2.charAt(i), gen1.substring(1), gen2.substring(0, i) + gen2.substring(i + 1), combinaciones);
+        }
+    }
 }
