7f73f701c688f76fcb7294a924b47f8afa6580b9
KravAD <dkrav@myuax.com>
2024-04-15 19:54:46 +0200

    commit 11
    Método de sumatoria que para valores mayores a 0 hará la suma n+(n-1).  El método listarNumeros hará una lista si el valor de start > end.

--- AnalisisGenomicoYOrganizacionDatos/src/main/java/AnalisisNumerico/SumYListaNum.java
+++ AnalisisGenomicoYOrganizacionDatos/src/main/java/AnalisisNumerico/SumYListaNum.java
@@ -1,4 +1,19 @@
 package AnalisisNumerico;
 
 public class SumYListaNum {
+    public int sumatoria(int n) {
+        if (n <= 0) {
+            return 0;
+        } else {
+            return n + sumatoria(n - 1);
+        }
+    }
+    public void listarNumeros(int start, int end) {
+        if (start > end) {
+            return;
+        } else {
+            System.out.println(start);
+            listarNumeros(start + 1, end);
+        }
+    }
 }
