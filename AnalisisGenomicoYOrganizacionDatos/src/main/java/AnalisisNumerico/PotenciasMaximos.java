d66df7b4a936f76bfa4faabbaf6bc9e3662ed7c0
KravAD <dkrav@myuax.com>
2024-04-15 21:15:50 +0200

    commit 13
    El método potencia devuelve el cálculo si la potencia es distinta de 0. El método máximo recorre los index del array para encontrar el valor máximo.

--- AnalisisGenomicoYOrganizacionDatos/src/main/java/AnalisisNumerico/PotenciasMaximos.java
+++ AnalisisGenomicoYOrganizacionDatos/src/main/java/AnalisisNumerico/PotenciasMaximos.java
@@ -1,4 +1,22 @@
 package AnalisisNumerico;
 
 public class PotenciasMaximos {
+
+
+    public int potencia(int base, int exponente) {
+        if (exponente == 0) {
+            return 1;
+        } else {
+            return base * potencia(base, exponente - 1);
+        }
+    }
+
+    public int maximo(int[] array, int index) {
+        if (index == array.length - 1) {
+            return array[index];
+        } else {
+            return Math.max(array[index], maximo(array, index + 1));
+        }
+    }
 }
+
