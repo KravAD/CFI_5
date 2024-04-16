93e285097c211e3927562407142d5e87c0159d76
KravAD <dkrav@myuax.com>
2024-04-16 19:34:07 +0200

    commit 22
    Atributos de fecha y resultado del experimento. Constructor de la clase y getters.

--- AnalisisGenomicoYOrganizacionDatos/src/main/java/GestionInfo/Experimento.java
+++ AnalisisGenomicoYOrganizacionDatos/src/main/java/GestionInfo/Experimento.java
@@ -1,4 +1,21 @@
 package GestionInfo;
 
+import java.time.LocalDate;
+
 public class Experimento {
+    private LocalDate fecha;
+    private String resultado;
+
+    public Experimento(LocalDate fecha, String resultado) {
+        this.fecha = fecha;
+        this.resultado = resultado;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public String getResultado() {
+        return resultado;
+    }
 }
