c5d0deb949841d305e288d8e9794deed1b2eca4f
KravAD <dkrav@myuax.com>
2024-04-16 18:54:45 +0200

    commit 19
    El método busquedaLineal crea un lista donde mete las palabras del texto y las separa con la función split para luego comprobar una por una si hay alguna que sea la que se busca. El método busquedaBinaria es igual solo que implementa collections para poder usar binarySearch  que devuelve un true si la palabra se encuentra y en caso contrario devuelve false.

--- AnalisisGenomicoYOrganizacionDatos/src/main/java/GestionInfo/Buscar.java
+++ AnalisisGenomicoYOrganizacionDatos/src/main/java/GestionInfo/Buscar.java
@@ -1,4 +1,24 @@
 package GestionInfo;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Buscar {
+    public static boolean busquedaLineal(String texto, String palabraBuscada) {
+        List<String> palabras = Arrays.asList(texto.split("\\s+"));
+        for (String palabra : palabras) {
+            if (palabra.equals(palabraBuscada)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean busquedaBinaria(String texto, String palabraBuscada) {
+        List<String> palabras = Arrays.asList(texto.split("\\s+"));
+        Collections.sort(palabras);
+        int indice = Collections.binarySearch(palabras, palabraBuscada);
+        return indice >= 0;
+    }
 }
