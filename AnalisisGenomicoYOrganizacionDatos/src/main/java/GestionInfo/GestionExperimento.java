65ea0ffd9affa7b7cd4ed71a98981b1efafd58ee
KravAD <dkrav@myuax.com>
2024-04-16 19:37:57 +0200

    commit 24
    Importo Lista, ArrayList, comparador y atributo de enlistar.

--- AnalisisGenomicoYOrganizacionDatos/src/main/java/GestionInfo/GestionExperimento.java
+++ AnalisisGenomicoYOrganizacionDatos/src/main/java/GestionInfo/GestionExperimento.java
@@ -1,4 +1,10 @@
 package GestionInfo;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class GestionExperimento {
+    private List<Experimento> experimentos;
+
 }
